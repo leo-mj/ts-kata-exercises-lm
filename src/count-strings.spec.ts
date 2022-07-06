bb786219682bde05ef4122c9dcbe1ca5ca43dbf2
Leo <leo.maedje@gmail.com>
2022-07-06 14:07:06 +0100

    Wrote code and test for countStrings

---
+++ src/count-strings.spec.ts
@@ -0,0 +1,7 @@
+import countStrings from "./count-strings";
+
+test("counts all strings in array of numbers and strings", () => {
+  expect(countStrings(['old', 10, 20, 'pond', -5, -3])).toBe(2);
+  expect(countStrings([10, 20, -5, -3])).toBe(0);
+  expect(countStrings(['2', 10, 20, 'pond', -5, -3])).toBe(2);
+});
---
+++ src/count-strings.ts
@@ -0,0 +1,15 @@
+/*
+Return the count of all strings in an array of numbers and strings.  Use array functions, not a loop. 
+*/
+/**
+ * returns the count of all strings in a given array of numbers and strings
+ * @param a - the first number to add
+ * @param b - the second number to add
+ * @returns the total
+ */
+function countStrings(inputArray: (number | string)[]): number {
+  const stringArray = inputArray.filter(element => typeof(element)=== "string");
+  return stringArray.length;
+}
+
+export default countStrings;
