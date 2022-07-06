0a8fc9b07bbc5a8ce787a693ebd08d1cab3e3fe7
Leo <leo.maedje@gmail.com>
2022-07-06 15:37:58 +0100

    Refactored which-are-in code and added tests to its test file

--- src/which-are-in.ts
+++ src/which-are-in.ts
@@ -3,31 +3,43 @@
  * of the strings of a1 which are substrings of strings of a2.
  * @param {string[]} a1 - the first array
  * @param {string[]} a2 - the second array
- * @returns {string[]} - the sorted array of strings
+ * @returns {string[]} - the array of sorted strings of a1 that are substrings of a string of a2
  */
 function areIn(a1: string[], a2: string[]): string[] {
-  const sortedArray: any[] = [];
-  for (let element of a1) {
-    if (isIn(element, a2)) {
-      sortedArray.push(element);
-    }
-  }
+  const sortedArray: string[] = a1.filter(searchStr => isIn(searchStr,a2));
   sortedArray.sort();
-  return sortedArray;
+  const uniqueArray: any[] = removeDuplicates(sortedArray);
+  return uniqueArray;
 }
 
 /**
- * Given a string and an array of strings, returns true if the string is a substring of any string in the array
- * @param {string} searchStr - string to search for
+ * Given a string and an array, returns true if the string is a substring of any string in the array
+ * @param {string[]} searchStr - string to search for
  * @param {string[]} strArray - array to search in
+ * @returns {string[]} - array of strings that are substrings
  */
 function isIn(searchStr: string, strArray: string[]): boolean {
-  for (let element of strArray) {
-    if (element.search(searchStr) != -1) {
+  for (let str of strArray) {
+    if (str.search(searchStr) != -1) {
       return true;
     }
   }
   return false;
+}
+
+/**
+ * Given an array, returns a copy of that array with duplicates removed.
+ * @param {string[]} strArray - sorted array with potential duplicates
+ * @returns {string[]} - array without duplicates
+ */
+function removeDuplicates(strArray: string[]): string[] {
+  const uniqueArray: string[] = [];
+  for (let str of strArray) {
+    if (!uniqueArray.includes(str)) {
+      uniqueArray.push(str);
+    }
+  }
+  return uniqueArray;
 }
 
 export default areIn;
